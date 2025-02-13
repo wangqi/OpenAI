c94e79f8d21f60f81c4c8da4f0f45500f6d41d21
wangqi <wang.qi@qikuyx.com>
2025-02-13 16:08:40 +0800

    Add gpt4_o to models in Demo/DemoChat/Sources/UI/DetailView.swift

--- Demo/DemoChat/Sources/UI/DetailView.swift
+++ Demo/DemoChat/Sources/UI/DetailView.swift
@@ -20,7 +20,7 @@ struct DetailView: View {
     @State private var selectedChatModel: Model = .gpt4_0613
     var availableAssistants: [Assistant]
 
-    private static let availableChatModels: [Model] = [.gpt3_5Turbo, .gpt4]
+    private static let availableChatModels: [Model] = [.gpt4_o, .gpt4_o_mini, .o1_mini, .o3_mini]
 
     let conversation: Conversation
     let error: Error?
