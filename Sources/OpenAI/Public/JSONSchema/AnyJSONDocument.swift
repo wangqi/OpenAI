5d1184cd4b97bb63352b880b6c2a8173d487fce6
wangqi <wang.qi@qikuyx.com>
2025-05-28 20:50:38 -0400

    Feature: DeepSeek expect number to be decoded and encoded as Double instead of Decimal. I add Double to JSONDocument

--- Sources/OpenAI/Public/JSONSchema/AnyJSONDocument.swift
+++ Sources/OpenAI/Public/JSONSchema/AnyJSONDocument.swift
@@ -27,6 +27,9 @@ public struct AnyJSONDocument: JSONDocument {
             self.value = intVal
         } else if let decimalVal = try? container.decode(Decimal.self) {
             self.value = decimalVal
+        //wangqi 2025-05-28
+        } else if let doubleVal = try? container.decode(Double.self) {
+            self.value = doubleVal
         } else if let boolVal = try? container.decode(Bool.self) {
             self.value = boolVal
         } else if let stringVal = try? container.decode(String.self) {
@@ -46,6 +49,8 @@ extension AnyJSONDocument: Equatable {
         switch (lhs.value, rhs.value) {
         case (let l as Int, let r as Int): return l == r
         case (let l as Decimal, let r as Decimal): return l == r
+        //wangqi 2025-05-28
+        case (let l as Double, let r as Double): return l == r
         case (let l as String, let r as String): return l == r
         case (let l as Bool, let r as Bool): return l == r
         case (_ as JSONNullValue, _ as JSONNullValue): return true
@@ -71,17 +76,20 @@ extension AnyJSONDocument: Hashable {
             hasher.combine(0); hasher.combine(v)
         case let v as Decimal:
             hasher.combine(1); hasher.combine(v)
-        case let v as String:
-            hasher.combine(2); hasher.combine(v)
-        case let v as Bool:
+        //wangqi 2025-05-28
+        case let v as Double:
             hasher.combine(3); hasher.combine(v)
+        case let v as String:
+            hasher.combine(4); hasher.combine(v)
+        case let v as Bool:
+            hasher.combine(5); hasher.combine(v)
         case _ as JSONNullValue:
-            hasher.combine(4)
+            hasher.combine(6)
         case let v as [any JSONDocument]:
-            hasher.combine(5)
+            hasher.combine(7)
             v.forEach { hasher.combine(AnyJSONDocument($0)) }
         case let v as [String: any JSONDocument]:
-            hasher.combine(6)
+            hasher.combine(8)
             for (k, v) in v.sorted(by: { $0.key < $1.key }) {
                 hasher.combine(k)
                 hasher.combine(AnyJSONDocument(v))
